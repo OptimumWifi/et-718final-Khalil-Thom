245f0101c4cd0c854871d0b89bccd53bba13017d
EC2 Default User <ec2-user@ip-10-16-0-70.ec2.internal>
2023-05-16 17:53:25 +0000

    Final Finished

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -5,6 +5,15 @@ class ArticlesController < ApplicationController
 def new
   @article = Article.new
 end
+
+def index
+    @articles = Article.all
+end
+ 
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
  
 def edit
   @article = Article.find(params[:id])
